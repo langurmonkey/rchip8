17a769f863b062206bd4a7b17fcfe2068f957101
langurmonkey <me@tonisagrista.com>
2021-04-15 20:21:18 +0200

    feat: sdl2

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ mod constants;
 mod debug;
 mod time;
 
+extern crate sdl2;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::{env, process};
 
 use chip8::Chip8;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
 // Simple CHIP8 emulator
 // MIT license
@@ -31,6 +37,22 @@ fn main() {
 
     // Create the machine
     let mut chip8 = Chip8::initialize(rom);
+
+    // let sdl_context = sdl2::init().unwrap();
+    // let video_subsystem = sdl_context.video().unwrap();
+
+    // let window = video_subsystem
+    //     .window("rust-sdl2 demo", 800, 600)
+    //     .position_centered()
+    //     .build()
+    //     .unwrap();
+
+    // let mut canvas = window.into_canvas().build().unwrap();
+
+    // canvas.set_draw_color(Color::RGB(0, 255, 255));
+    // canvas.clear();
+    //canvas.present();
+
     // Run the machine
     chip8.run();
 }
